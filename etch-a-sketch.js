48f9f6366f87bd8bd7ec39f3eacebc6f97d517f5
mnick02 <nokiamn3@gmail.com>
2025-01-29 13:40:06 -0800

    Change grid implementation to rows and columns
    Changed the grid implementation to two separate
    divs "row" and "column", giving me more control
    of the formatting of the grid. This was achieved
    by setting up two different loops, with both
    creating 16 divs each for the rows and columns

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,17 @@
 const container = document.querySelector("#container");
 
 //Used 256 since 16 * 16 = 256
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("content");
-    square.setAttribute("style", "border: solid, black");
-    square.style.height = "10px";
-    square.style.width = "10px";
-    container.appendChild(square);
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        column.setAttribute("style", "border: solid, black");
+        column.style.height = "10px";
+        column.style.width = "10px";
+        row.appendChild(column);
+    }
 }
+
