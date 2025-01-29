8e9edbd92d39cb83f28797722488708c530c0125
mnick02 <nokiamn3@gmail.com>
2025-01-29 14:15:17 -0800

    Remove styling for column div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 
-//Used 256 since 16 * 16 = 256
+
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -8,9 +8,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const column = document.createElement("div");
         column.classList.add("column");
-        column.setAttribute("style", "border: solid, black");
-        column.style.height = "10px";
-        column.style.width = "10px";
+        //column.setAttribute("style", "border: solid, black");
+        //column.style.height = "10px";
+        //column.style.width = "10px";
         row.appendChild(column);
     }
 }
