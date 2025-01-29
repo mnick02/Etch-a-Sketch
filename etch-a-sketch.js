c580be57bf5fde3a7938256958870ca577ae8cc5
mnick02 <nokiamn3@gmail.com>
2025-01-29 12:41:53 -0800

    Change square div styling

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,7 @@ const container = document.querySelector("#container");
 
 const square = document.createElement("div");
 square.classList.add("content");
-square.setAttribute("style", "border: thick, solid, black; background: pink");
-square.style.height = "200px";
-square.style.width = "200px";
+square.setAttribute("style", "border: solid, black");
+square.style.height = "50px";
+square.style.width = "50px";
 container.appendChild(square);
