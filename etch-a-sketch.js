e96d01887978aaeba31871674878d34b5db7c104
mnick02 <nokiamn3@gmail.com>
2025-01-29 12:40:08 -0800

    Create square div styling

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,4 +2,7 @@ const container = document.querySelector("#container");
 
 const square = document.createElement("div");
 square.classList.add("content");
+square.setAttribute("style", "border: thick, solid, black; background: pink");
+square.style.height = "200px";
+square.style.width = "200px";
 container.appendChild(square);
