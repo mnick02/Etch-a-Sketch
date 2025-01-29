5cc045981104d9ba8e58466b06f756f8eb24148a
mnick02 <nokiamn3@gmail.com>
2025-01-29 12:47:31 -0800

    Add enough square divs for 16x16 grid
    Added enough square divs for the 16x16 grid by
    setting up a for loop; also change the size of
    square divs so they can fit better

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,11 @@
 const container = document.querySelector("#container");
 
-const square = document.createElement("div");
-square.classList.add("content");
-square.setAttribute("style", "border: solid, black");
-square.style.height = "50px";
-square.style.width = "50px";
-container.appendChild(square);
+//Used 256 since 16 * 16 = 256
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("content");
+    square.setAttribute("style", "border: solid, black");
+    square.style.height = "10px";
+    square.style.width = "10px";
+    container.appendChild(square);
+}
