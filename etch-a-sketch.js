589d70c520ce40f364e764f7d9198134bf529001
mnick02 <nokiamn3@gmail.com>
2025-01-30 12:11:21 -0800

    Add removeGrid and createGrid function
    Added these two functions to help with the
    creation of new grids. removeGrid sets innerHTML
    of container to nothing, removing the grid;
    createGrid is the same as the old code, but now a
    function allowing for users to change its size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,58 @@
 const container = document.querySelector("#container");
 
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        //column.setAttribute("style", "border: solid, black");
-        //column.style.height = "10px";
-        //column.style.width = "10px";
-        row.appendChild(column);
-        column.addEventListener("mouseover", (event) => {
-            console.log("hello");
-            column.style.backgroundColor = "blue";
-        });
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+            column.addEventListener("mouseover", (event) => {
+                //console.log("hello");
+                column.style.backgroundColor = "blue";
+            });
+        }
     }
 }
 
+//FIND OUT HOW TO REMOVE DIVS FIRST
+
+
 const button = document.querySelector("button");
 button.addEventListener("click", (event) => {
-    prompt("Number of squares per side: ")
+    let userinput = prompt("Number of squares per side: ")
+    console.log(userinput);
+    if (userinput > 100 || userinput <= 0 || isNaN(userinput) === true) {
+        alert("Sorry, invalid number of sides")
+    }
+    else {
+        console.log("here");
+        //divs = document.querySelectorAll("column").remove();
+        removeGrid();
+        createGrid(userinput);
+        console.log("below");
+        /*for (let i = 0; i < userinput; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            container.appendChild(row);
+            for (let j = 0; j < userinput; j++) {
+                const column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+                column.addEventListener("mouseover", (event) => {
+                    //console.log("hello");
+                    column.style.backgroundColor = "blue";
+                });
+            }
+        }*/
+
+    }
 });
 
+function removeGrid(){
+    container.innerHTML = '';
+}
+
+createGrid(16);
